ac38905cf8357ab10a52ccff360cbf9a04f550f3
Capitaine-Vole <capitainevole@gmail.com>
2023-06-01 00:39:41 +0100

    changing all variables to pointers

--- main.cpp
+++ main.cpp
@@ -1,44 +1,48 @@
 #include <iostream>
 using namespace std;
 
-void calcTime(int seconds) {
-	int days = 0;
-	int hours = 0;
-	int minutes = 0;
-	int secs = 0;
+void calcTime(int* seconds) {
+	int* days = (int*)malloc(sizeof(int));
+	int* hours = (int*)malloc(sizeof(int));
+	int* minutes = (int*)malloc(sizeof(int));
+	int* secs = (int*)malloc(sizeof(int));
+	*days = 0;
+	*hours = 0;
+	*minutes = 0;
+	*secs = 0;
 
-	if (int(seconds)>0) {
-		days = int( seconds / (24*60*60));
-		seconds -= days * (24 * 60 * 60);
+	if (int(*seconds)>0) {
+		*days = int( *seconds / (24*60*60));
+		*seconds -= *days * (24 * 60 * 60);
 	}
-	if (int(seconds) > 0) {
-		hours = int(seconds / (60 * 60));
-		seconds -= hours * (60 * 60);
+	if (int(*seconds) > 0) {
+		*hours = int(*seconds / (60 * 60));
+		*seconds -= *hours * (60 * 60);
 	}
-	if (int(seconds) > 0) {
-		minutes = int(seconds / 60);
-		seconds -= minutes * 60;
+	if (int(*seconds) > 0) {
+		*minutes = int(*seconds / 60);
+		*seconds -= *minutes * 60;
 	}
-	if (int(seconds) > 0) {
-		secs = seconds;
+	if (int(*seconds) > 0) {
+		*secs = *seconds;
 	}
-	if (int(seconds) < 0) {
+	if (int(*seconds) < 0) {
 		cout << "There was a problem while calculating time" << endl;
 		return;
 	}
 
-	cout << days << " days" << endl;
-	cout << hours << " hours" << endl;
-	cout << minutes << " minutes" << endl;
-	cout << secs << " seconds" << endl;
+	cout << *days << " days" << endl;
+	cout << *hours << " hours" << endl;
+	cout << *minutes << " minutes" << endl;
+	cout << *secs << " seconds" << endl;
 
 }
 
 int main() {
 
-	int sc;
+	int* sc = (int*) malloc(sizeof(int));
 	cout << "Enter the amount of seconds you want to calculate the time : ";
-	cin >> sc;
+	cin >> *sc;
 	cin.ignore();
 
 	calcTime(sc);
